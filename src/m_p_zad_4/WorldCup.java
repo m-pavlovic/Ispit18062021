367e0a28a5381668e2db4c0dd81e3362dd0f8e20
m-pavlovic <marija.pavlovic19@gmail.com>
2023-06-18 15:51:19 +0200

    4. zad

--- src/m_p_zad_4/WorldCup.java
+++ src/m_p_zad_4/WorldCup.java
@@ -1,6 +1,8 @@
 package m_p_zad_4;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -33,31 +35,63 @@ public class WorldCup {
             System.out.println(s);
         }
     }
-//
-    public void save2File(String fileName) {
-        File file = new File(fileName);
-        try {
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-            System.out.println("File created");
-        } catch (Exception e) {
-            System.out.println("Error creating file");
-        }
+/*
+Enter a year:
+1982
 
+[USA, Sweeden, Island, Australia, Spain, Turkey]
+Participants in the WorldCup - 1982
+USA
+Sweeden
+Island
+Australia
+Spain
+Turkey
+
+ */
+    public void save2File(String fileName) {
+//        File file = new File(fileName);
+//        FileOutputStream fos = null;
+//        try {
+//            fos = new FileOutputStream(file, true);
+//            String str = "Participants in the WorldCup - " + year + "\n";
+//            byte[] b = str.getBytes();
+//            fos.write(b);
+//            for (String s : part) {
+//                b = s.getBytes();
+//                fos.write(b);
+//                fos.write('\n');
+//            }
+//            fos.close();
+//        } catch (Exception e) {
+//            System.out.println("Error: " + e.getMessage());
+//        }
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName));
+            oos.writeObject(part);
+            oos.close();
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
 
     }
 
     public void read4File(String fileName) {
         File file = new File(fileName);
+        Scanner sc = null;
         try {
             sc = new Scanner(file);
+            System.out.println("-----------------------------------------------------------");
+            System.out.println("From file " + fileName + ":");
+            System.out.println("-----------------------------------------------------------");
             while (sc.hasNextLine()) {
                 System.out.println(sc.nextLine());
             }
+            sc.close();
         } catch (Exception e) {
-            System.out.println("Error reading file");
+            System.out.println("Error: " + e.getMessage());
         }
+
 
     }
 
